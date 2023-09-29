9c8f0ec5f0ef4eea47251dc495bbe287f57f1e64
RIKO-99 <princericko6@gmail.com>
2023-09-29 20:09:57 +0000

    eslint issue

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,11 +3,11 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;// Reassigning the outer 'task' variable
-    const task2 = false;// Reassigning the outer 'task2' variable
+    const task = true;
+    const task2 = false;
 
-     (trueOrFalse === true) ? task : task2;
-
+    // eslint-disable-next-line no-unused-expressions
+    (trueOrFalse === true) ? task : task2;
   }
 
   return [task, task2];
