6477a85c63496fb03c216637ce336f7004ba45b5
RIKO-99 <princericko6@gmail.com>
2023-09-29 19:25:35 +0000

    const and hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;// Reassigning the outer 'task' variable
-    task2 = false;// Reassigning the outer 'task2' variable
+    const task = true;// Reassigning the outer 'task' variable
+    const task2 = false;// Reassigning the outer 'task2' variable
   }
 
   return [task, task2];
